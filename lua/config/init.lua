5ee1d00c5921bd412a0c38c65da7b58b29ee1a5d
renms <renms@mail.ru>
2024-05-27 21:22:02 +0400

    lazygit

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -239,6 +239,7 @@ require("lazy").setup({
 
 	-- "gc" to comment visual regions/lines
 	{ "numToStr/Comment.nvim", opts = {} },
+	require("plugins.lazygit"),
 
 	-- Here is a more advanced example where we pass configuration
 	-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
